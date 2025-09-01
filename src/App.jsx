b396496baaeddc25a28e25b5fc5db6ecaac115e3
Jyotish yadav <22bcs12392@cuchd.in>
2025-09-01 21:10:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
 import Home from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
